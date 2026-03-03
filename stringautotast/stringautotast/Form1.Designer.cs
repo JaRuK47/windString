0283254e9e53eb3a1fbf99b078db7b375dd01d11
KuRaJ <Kuraj.com>
2026-03-03 13:01:43 +0800

    working form window

--- stringautotast/stringautotast/Form1.Designer.cs
+++ stringautotast/stringautotast/Form1.Designer.cs
@@ -28,67 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             button1 = new Button();
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
+            firstWord = new TextBox();
+            secondWord = new TextBox();
             label1 = new Label();
             label2 = new Label();
+            label3 = new Label();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(330, 26);
+            button1.Location = new Point(325, 143);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
             button1.Text = "Готоува";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
-            // textBox1
+            // firstWord
             // 
-            textBox1.Location = new Point(176, 10);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(125, 27);
-            textBox1.TabIndex = 1;
+            firstWord.Location = new Point(171, 127);
+            firstWord.Name = "firstWord";
+            firstWord.Size = new Size(125, 27);
+            firstWord.TabIndex = 1;
             // 
-            // textBox2
+            // secondWord
             // 
-            textBox2.Location = new Point(176, 43);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(125, 27);
-            textBox2.TabIndex = 2;
+            secondWord.Location = new Point(171, 160);
+            secondWord.Name = "secondWord";
+            secondWord.Size = new Size(125, 27);
+            secondWord.TabIndex = 2;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(26, 15);
+            label1.Location = new Point(21, 132);
             label1.Name = "label1";
             label1.Size = new Size(64, 20);
             label1.TabIndex = 3;
             label1.Text = "Слово 1";
-            label1.Click += label1_Click;
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(26, 43);
+            label2.Location = new Point(21, 160);
             label2.Name = "label2";
             label2.Size = new Size(64, 20);
             label2.TabIndex = 4;
             label2.Text = "Слово 2";
             // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(502, 100);
+            label3.TabIndex = 5;
+            label3.Text = resources.GetString("label3.Text");
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(475, 97);
+            ClientSize = new Size(516, 205);
+            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
+            Controls.Add(secondWord);
+            Controls.Add(firstWord);
             Controls.Add(button1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "String задание";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,9 +108,10 @@
         #endregion
 
         private Button button1;
-        private TextBox textBox1;
-        private TextBox textBox2;
+        private TextBox firstWord;
+        private TextBox secondWord;
         private Label label1;
         private Label label2;
+        private Label label3;
     }
 }
--- stringautotast/stringautotast/Form1.cs
+++ stringautotast/stringautotast/Form1.cs
@@ -7,9 +7,53 @@ namespace stringautotast
             InitializeComponent();
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            string first, second;
+            try
+            {
+                first = this.firstWord.Text.ToLower();
+                second = this.secondWord.Text.ToLower();
+            }
+            catch (FormatException)
+            {
+                return;
+            }
+            List<string> results = Logic.Result(first, second);
+            MessageBox.Show(string.Join(" ", results));
+        }
+    }
 
+    public class Logic
+    {
+        public static List<string> Result(string first, string second)
+        {
+            var simvolF = new HashSet<char>();
+            var simvolS = new HashSet<char>();
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                simvolF.Add(first[i]);
+            }
+            for (int i = 0; i < second.Length; i++)
+            {
+                simvolS.Add(second[i]);
+            }
+
+            List<string> results = new List<string>();
+
+            foreach (char c in simvolF)
+            {
+                if (simvolS.Contains(c))
+                {
+                    results.Add("да");
+                }
+                else
+                {
+                    results.Add("нет");
+                }
+            }
+            return results;
         }
     }
 }
